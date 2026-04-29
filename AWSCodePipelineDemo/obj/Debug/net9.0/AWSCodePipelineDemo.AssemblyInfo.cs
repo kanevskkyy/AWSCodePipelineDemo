0a800e8ade9c4ec5d22c84802e769cf2d9053815
kanevskkyy <oleksandrkanovskij@gmail.com>
2026-04-29 11:25:02 +0300

    Add Swager

--- AWSCodePipelineDemo/obj/Debug/net9.0/AWSCodePipelineDemo.AssemblyInfo.cs
+++ AWSCodePipelineDemo/obj/Debug/net9.0/AWSCodePipelineDemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AWSCodePipelineDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a22f5430ae61c5c8fbf887cc0522698151f3eb25")]
 [assembly: System.Reflection.AssemblyProductAttribute("AWSCodePipelineDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AWSCodePipelineDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
